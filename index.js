e562a1d139232aa14b491d34c8f4ac2ca77dc037
Wing <winghangsong@gmail.com>
2023-01-25 00:06:33 -0500

    MPBG1

--- index.js
+++ index.js
@@ -2,3 +2,26 @@ let playerSelection;
 let computerSelection;
 let result;
 
+function playGame(playerSelection) {
+  computerSelection = Math.random();
+  if (computerSelection < 0.34) {
+    computerSelection = "rock";
+  } else if (computerSelection <= 0.67) {
+    computerSelection = "paper";
+  } else {
+    computerSelection = "scissors";
+  }
+
+  if (playerSelection == computerSelection) {
+    result = "tie";
+  } else if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    result = "win";
+  } else {
+    result = "lose";
+  }
+  document.getElementById("result").innerHTML = `You ${result}! You chose ${playerSelection} and the computer chose ${computerSelection}.`;
+}
